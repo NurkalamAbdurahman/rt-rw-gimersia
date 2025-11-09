c40387c433d3a55526837340df185c6c027e5162
naufalsansan <naufalikhsan50@gmail.com>
2025-11-09 16:01:50 +0700

    PAUSE MENU

--- Scenes/pause.gd
+++ Scenes/pause.gd
@@ -1,44 +1,36 @@
 extends Control
 
+@export var pause_key: String = "esc"
+
 @onready var resume_button = $PanelContainer/VBoxContainer/resume
 @onready var restart_button = $PanelContainer/VBoxContainer/restart
-@onready var controls_button = $PanelContainer/VBoxContainer/controls
 @onready var quit_button = $PanelContainer/VBoxContainer/quit
 
-func _ready():
-	visible = false  # menu pause awalnya tersembunyi
+func _ready() -> void:
+	if resume_button:
+		resume_button.pressed.connect(_on_resume_pressed)
+	if restart_button:
+		restart_button.pressed.connect(_on_restart_pressed)
+	if quit_button:
+		quit_button.pressed.connect(_on_quit_pressed)
 
-	# hubungkan tombol
-	resume_button.pressed.connect(_on_resume_pressed)
-	restart_button.pressed.connect(_on_restart_pressed)
-	controls_button.pressed.connect(_on_controls_pressed)
-	quit_button.pressed.connect(_on_quit_pressed)
-
-func _process(delta):
-	# toggle pause menu dengan Esc
-	if Input.is_action_just_pressed("esc"):
-		if visible:
-			hide_pause()
-		else:
-			show_pause()
-
-func show_pause():
-	visible = true
-	get_tree().paused = true
-
-func hide_pause():
 	visible = false
-	get_tree().paused = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(pause_key):
+		toggle_pause()
+
+func toggle_pause():
+	get_tree().paused = not get_tree().paused
+	visible = get_tree().paused
 
 func _on_resume_pressed():
-	hide_pause()
+	get_tree().paused = false
+	visible = false
 
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
-
-func _on_controls_pressed():
-	print("Controls button pressed")  # nanti dihubungkan ke tampilan controls
 
 func _on_quit_pressed():
 	get_tree().quit()
