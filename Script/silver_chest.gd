bee610d6a381a74516ac37947a4f14eab8222778
naufalsansan <naufalikhsan50@gmail.com>
2025-11-09 12:40:58 +0700

    silver chest +  pause menu

--- Script/silver_chest.gd
+++ Script/silver_chest.gd
@@ -1,38 +1,54 @@
 extends Node2D
-
-@onready var tertutup = $silver_chest
-@onready var animasi = $silver_chest_openanimation
-@onready var terbuka = $silver_chest_open
-@onready var area = $Area2D
-@onready var label = $Label
+@onready var tertutup: Sprite2D = $silver_chest
+@onready var anim_sprite: AnimatedSprite2D = $silver_chest_openanimation
+@onready var terbuka: Sprite2D = $silver_chest_open
+@onready var area: Area2D = $Area2D
+@onready var label: Label = $Label
 
 var player_in_area = false
-var chest_opened = false  # cek apakah chest sudah dibuka
+var chest_opened = false
+
 
 func _ready():
 	tertutup.visible = true
 	terbuka.visible = false
+	anim_sprite.visible = false
+	anim_sprite.stop()
 	label.visible = false
-	animasi.play("open")  # pastikan animasi default
-	animasi.stop()        # hentikan supaya tidak looping
+	
+func _process(delta):
+	if player_in_area and not chest_opened:
+		if Input.is_action_just_pressed("e"):
+			buka_chest()
 
-# Signal body entered
+
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not chest_opened:
 		player_in_area = true
 		label.visible = true
-		buka_chest()
 
-# Signal body exited
+
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_area = false
 		label.visible = false
-		# Fungsi buka chest
+
 func buka_chest():
 	chest_opened = true
 	label.visible = false
+
 	tertutup.visible = false
+	anim_sprite.visible = true
+
+	# mainkan animasi sekali saja
+	anim_sprite.animation = "open"
+	anim_sprite.play()
+
+	# tunggu sampai animasi selesai
+	await anim_sprite.animation_finished
+
+	# setelah animasi selesai, sembunyikan animasi dan tampilkan sprite chest terbuka
+	anim_sprite.visible = false
 	terbuka.visible = true
-	animasi.play("open")  # pastikan animasi hanya dimainkan sekali
