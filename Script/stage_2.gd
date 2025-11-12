3c4bbf6f5a33951ea313b2c5afec3841027c032e
Sandi Virgiawan <sandivirgiawan64@gmail.com>
2025-11-12 18:00:58 +0700

    fix stage 2

--- Script/stage_2.gd
+++ Script/stage_2.gd
@@ -1,4 +1,4 @@
-extends Node2D # Root node dari maze-01.tscn
+extends Node2D
 
 # ⭐️ Perubahan di sini: Menggunakan $Player2 sesuai Scene Tree Anda
 @onready var player_node = $Player2 
@@ -7,10 +7,6 @@ extends Node2D # Root node dari maze-01.tscn
 # Ganti angka di bawah ini dengan koordinat X dan Y yang tepat di mana Anda ingin 
 # karakter 'Player2' muncul saat keluar dari pintu besi.
 const IRON_DOOR_SPAWN_POSITION = Vector2(1406, 387) 
-@onready var tuta: Area2D = $tutorial_welcome
-@onready var tutb: Area2D = $tutorial_interract
-@onready var tutc: Area2D = $tutorial_goal
-@onready var tutd: Area2D = $tutorial_chest
 
 func _ready():
 	
@@ -23,10 +19,3 @@ func _ready():
 		print(GameData.next_spawn_location)
 		# 3. Reset variabel global
 		GameData.next_spawn_location = "" 
-		
-	# Lanjutkan dengan kode _ready() lainnya (jika ada)
-	if GameData.has_dead == true:
-		tuta.queue_free()
-		tutb.queue_free()
-		tutc.queue_free()
-		tutd.queue_free()
