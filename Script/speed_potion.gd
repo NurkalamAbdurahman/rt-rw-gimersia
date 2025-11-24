52e9cb6e01fa26ddb8966d36201ac7a42d4e8092
nurkalamaz <129570069+NurkalamAbdurahman@users.noreply.github.com>
2025-11-24 09:23:35 +0700

    prototype buff potion

--- Script/speed_potion.gd
+++ Script/speed_potion.gd
@@ -4,8 +4,8 @@ extends Control
 @onready var backgroung_menu: Sprite2D = $BackgroungMenu
 @onready var label: Label = $Label
 
-var label_default_color := Color.WHITE
-var default_scale := Vector2(0.5, 0.5)
+var label_default_color: Color = Color.WHITE
+var default_scale: Vector2 = Vector2(0.5, 0.5)
 
 const ANIM_DURATION_FAST: float = 0.1
 const ANIM_DURATION_NORMAL: float = 0.15
@@ -18,34 +18,50 @@ func _ready() -> void:
 	default_scale = sprite_2d.scale
 
 func _process(_delta: float) -> void:
-	label.text = str(GameData.hp_potion)
+	label.text = str(GameData.speed_potion)
 	
-	if Input.is_action_just_pressed("ui_hp_potion"): 
-		if GameData.use_hp_potion():
-			print("darah nambah")
+	if Input.is_action_just_pressed("ui_speed_potion"): 
+		_try_use_speed_potion()
+
+func _try_use_speed_potion() -> void:
+	var player = get_tree().get_first_node_in_group("Player")
+	
+	if not player:
+		print("Player not found in scene!")
+		_play_error_animation()
+		return
+	
+	if player.has_method("apply_speed_potion"):
+		var success = player.apply_speed_potion()
+		
+		if success:
+			print("Speed Potion used successfully!")
 			_play_success_animation()
 		else:
-			print("nggak bisa minum potion")
+			print("Cannot use Speed Potion")
 			_play_error_animation()
+	else:
+		print("Player doesn't have apply_speed_potion method!")
+		_play_error_animation()
 
-func _play_success_animation():
+func _play_success_animation() -> void:
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(sprite_2d, "scale", default_scale * SUCCESS_SCALE_MULTIPLIER, ANIM_DURATION_NORMAL)
 	tween.tween_property(sprite_2d, "scale", default_scale, ANIM_DURATION_NORMAL)
 
-func _play_error_animation():
+func _play_error_animation() -> void:
 	_bounce_icon(sprite_2d)
 	_flash_label(label)
 
-func _bounce_icon(node: Node2D):
+func _bounce_icon(node: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property(node, "scale", default_scale * ERROR_SCALE_MAX, ANIM_DURATION_FAST)
 	tween.tween_property(node, "scale", default_scale * ERROR_SCALE_MIN, ANIM_DURATION_FAST)
 	tween.tween_property(node, "scale", default_scale, ANIM_DURATION_FAST)
 
-func _flash_label(lb: Label):
-	var tween := create_tween()
+func _flash_label(lb: Label) -> void:
+	var tween = create_tween()
 	tween.tween_property(lb, "modulate", Color(1, 0.2, 0.2), ANIM_DURATION_FAST)
 	tween.tween_property(lb, "modulate", label_default_color, ANIM_DURATION_NORMAL)
